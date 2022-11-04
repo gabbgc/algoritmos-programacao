1d144ab2dffa811e15df338f5c9f4332645a8917
gabbgc <111456981+gabbgc@users.noreply.github.com>
2022-11-04 16:55:48 -0300

    Aceita apenas inteiros positivos

--- Condicional/ex11.c
+++ Condicional/ex11.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    double x, y, z;
+    int x, y, z;
     int opcao;
     printf("Digite x : ");
-    scanf("%lf", &x);
+    scanf("%d", &x);
     printf("Digite y : ");
-    scanf("%lf", &y);
+    scanf("%d", &y);
     printf("Digite z : ");
-    scanf("%lf", &z);
-    printf("Digite a operacao desejada.\n");
-    printf("1 - Media geometrica.\n");
-    printf("2 - Media ponderada.\n");
-    printf("3 - Media harmonica.\n");
-    printf("4 - Media aritmetica.\n");
-    printf("Opcao : ");
-    scanf("%d", &opcao);
-    switch(opcao) {
-        case 1:
-            printf("%g", x * y * z);
-            break;
-        case 2:
-            printf("%g", (x + 2 * y + 3 * z) / 6);
-            break;
-        case 3:
-            printf("%g", 1/(1/x + 1/y + 1/z));
-            break;
-        case 4:
-            printf("%g", (x + y + z) / 3);
-            break;
-        default:
-            printf("Opcao invalida");
-            break;
+    scanf("%d", &z);
+    if(x > 0 && y > 0 && z > 0) {
+        printf("Digite a operacao desejada.\n");
+        printf("1 - Media geometrica.\n");
+        printf("2 - Media ponderada.\n");
+        printf("3 - Media harmonica.\n");
+        printf("4 - Media aritmetica.\n");
+        printf("Opcao : ");
+        scanf("%d", &opcao);
+        switch(opcao) {
+            case 1:
+                printf("%d", x * y * z);
+                break;
+            case 2:
+                printf("%g", ((double) x + 2 * (double) y + 3 * (double) z) / 6);
+                break;
+            case 3:
+                printf("%g", 1/(1/(double) x + 1/(double) y + 1/(double) z));
+                break;
+            case 4:
+                printf("%g", ((double) x + (double) y + (double) z) / 3);
+                break;
+            default:
+                printf("Opcao invalida");
+                break;
+        }
     }
+    else
+        printf("Por favor digite um numeros positivos.");
     return 0;
 }
